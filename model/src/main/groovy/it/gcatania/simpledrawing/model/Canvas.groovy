0a2fbdaf9e007f48576d3317a0fbe76cfc9a497c
Gabriele Catania <gabriele.ctn@gmail.com>
2015-08-03 00:08:07 +0200

    canvas bucket fill implementation

--- model/src/main/groovy/it/gcatania/simpledrawing/model/Canvas.groovy
+++ model/src/main/groovy/it/gcatania/simpledrawing/model/Canvas.groovy
@@ -65,9 +65,47 @@ class Canvas
         r.sides.each {add(it)}
     }
 
+    /**
+     * <p>bucket fill all points adjacent to the input point and having the same colour, changing them to the new colour.</p>
+     *
+     * <p>This method uses an iterative approach by keeping track of all "new" adjacent points in each iteration, and all
+     * the points found in all iterations to avoid iterating indefinitely.</p>
+     *
+     * @param p the point to start filling from
+     * @param colour the fill colour
+     */
     public void bucketFillFrom(Point p, char colour)
     {
-        //TODO add impl
+        validate(p);
+        char oldColour = getColourAt(p);
+        Set<Point> allPointsToFill = new HashSet<Point>();
+        Set<Point> newPointsToFill = Collections.singleton(p);
+        allPointsToFill.addAll(newPointsToFill);
+        while(!newPointsToFill.empty)
+        {
+            newPointsToFill = internalGetAdjacentPointsWithSameColour(allPointsToFill, newPointsToFill, oldColour);
+        }
+        allPointsToFill.each { setColour(it, colour); }
+    }
+
+    /**
+     * @param allPointsToFill the set of all points to be filled by the bucket fill
+     * @param newPointsToFill the set of all the "new" points found by looking for adjacents to existing points in the current iteration
+     * @param colour the colour of the adjacent points to find
+     * @return the set of the new points found (will also be added to the existing points)
+     */
+    private Set<Point> internalGetAdjacentPointsWithSameColour(Set<Point> allPointsToFill, Set<Point> newPointsToFill, char colour)
+    {
+        Set<Point> pointsFromPreviousStep = newPointsToFill;
+        newPointsToFill = new HashSet<Point>();
+        pointsFromPreviousStep.each(
+                { Point p ->
+                    Set<Point> adjacents = adjacents(p);
+                    Set<Point> withSameColour = adjacents.findAll { getColourAt(it) == colour };
+                    Set<Point> newOnes = withSameColour.findAll { allPointsToFill.add(it) };
+                    newPointsToFill.addAll(newOnes);
+                })
+        return newPointsToFill;
     }
 
     public char getColourAt(Point p)
@@ -105,5 +143,24 @@ class Canvas
         if(r== null) throw new IllegalArgumentException("No rectangle provided")
         validate(r.vertex1);
         validate(r.vertex2);
+    }
+
+    /**
+     * Note: this method is private here instead of public on the {@code Point} class to keep the latter as generic as possible
+     * @param p a point
+     * @return the set of all adjacent points inside the canvas (empty if the input point is outside the canvas)
+     */
+    private Set<Point> adjacents(Point p)
+    {
+        if(!contains(p))
+        {
+            return Collections.emptySet()
+        }
+        Point up = new Point(p.x, p.y-1);
+        Point down = new Point(p.x, p.y+1);
+        Point left = new Point(p.x-1, p.y);
+        Point right = new Point(p.x+1, p.y);
+        List<Point> containedAdjacents = [up, down, left, right].findAll({contains(it)});
+        return new HashSet<Point>(containedAdjacents)
     }
 }
